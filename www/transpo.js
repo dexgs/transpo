ee61c4f4959c56e908b2879ad15b5c9199b83904
transpo <transpo@nothing>
2021-04-13 23:01:30 -0400

    Visual fixes
    Restrict the length of the .file-name class so the x-button to remove a file is still visible for extremely long file names. Also change the sizeString function so the size reads 0B instead of NaN undefined when an empty file is added

--- www/transpo.js
+++ www/transpo.js
@@ -99,6 +99,9 @@ function updateFiles() {
 let units = ["B", "KB", "MB", "GB", "PB"];
 
 function sizeString(numBytes) {
+    if (numBytes ==  0) {
+        return "0B";
+    }
     var power = Math.floor(Math.log10(numBytes) / 3);
     if (power < 5) {
         return String((numBytes / Math.pow(10, power * 3)).toFixed(1)) + " " + units[power];
