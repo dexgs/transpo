319d2c8fca51366ce3efe38cfe1598947694db06
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2021-08-24 10:59:08 -0400

    move sizeString function into its own file

---
+++ www/size_string.js
@@ -0,0 +1,13 @@
+let units = ["B", "KB", "MB", "GB", "PB"];
+
+function sizeString(numBytes) {
+    if (numBytes ==  0) {
+        return "0B";
+    }
+    var power = Math.floor(Math.log10(numBytes) / 3);
+    if (power < 5) {
+        return String((numBytes / Math.pow(10, power * 3)).toFixed(1)) + " " + units[power];
+    } else {
+        return "Big";
+    }
+}
